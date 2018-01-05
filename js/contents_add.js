097af3f3405be9eb29de2facfc1b8ec1fd9fb143
Ghobe <ghobekang@gmail.com>
2018-01-06 02:50:59 +0900

    seach funciton logic change - there are so many trash values useless for searching function in content. so, when content is added, textonly feild is inserted additionally    all search function is implemented on the textonly feild.    but, already registered contents are unavailable to search someting.. i can't find a way to transform a html data into only text string.. if i find, i will update lately

--- js/contents_add.js
+++ js/contents_add.js
@@ -38,7 +38,7 @@ var functions = {
             })
     },
 
-    add : function (categories, subCategory, title, content, callback) {
+    add : function (categories, subCategory, title, content, contentTextOnly, callback) {
         const defaultApp = DefaultFuntionApp;
         const d = new Date();
         const createTime = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
@@ -54,6 +54,7 @@ var functions = {
                     seqNum: seqNum,
                     title: title,
                     content: content,
+                    textOnly: contentTextOnly,
                     created_time: createTime
                 });
             })
@@ -88,20 +89,6 @@ var functions = {
             if (typeof callback === 'function') {
                 callback(eachResult);
             }
-            // snapshot.forEach(function(childsnapshot) {
-            //     var key = childsnapshot.key;
-            //     var eachContentLocation = location.child(key);
-            //     // move the reference to content location(push unique key)
-            //     eachContentLocation.once('value').then(function(eachSnapshot) {
-            //         // refer to each unique key, get a Object through a method of val();
-            //         var eachData = eachSnapshot.val();
-            //
-            //         if (typeof callback === 'function') {
-            //             // callback
-            //             callback(eachData, totalContents);
-            //         }
-            //     })
-            // })
         })
     },
     loadRecentContents : function(category, limitTo, callback) {
@@ -156,29 +143,35 @@ var functions = {
                     .then(function(data) {
                         data.forEach(function(eachData) {
                             var resultOfSearch = [];
-                            if (eachData.val().title.search(searchFor) !== -1){
-                                var result = {
-                                    data:eachData.val(),
-                                    whereHit:"title",
-                                    indexOf:eachData.val().title.search(searchFor)
-                                };
-                                resultOfSearch.push(result);
+                            var content;
+                            console.log(eachData.val());
+                            if (eachData.hasChild('textOnly')) {
+                                content = eachData.val().textOnly;
 
-                            }else if(eachData.val().content.search(searchFor) !== -1){
-                                var result = {
-                                    data:eachData.val(),
-                                    whereHit:"content",
-                                    indexOf:eachData.val().content.search(searchFor)
-                                };
-                                resultOfSearch.push(result);
+                                if (eachData.val().title.search(searchFor) !== -1) {
+                                    var result = {
+                                        data: eachData.val(),
+                                        whereHit: "title",
+                                        indexOf: eachData.val().title.search(searchFor)
+                                    };
+                                    resultOfSearch.push(result);
 
-                            }else {
-                                console.log("not found");
+                                } else if (content.search(searchFor) !== -1) {
+                                    var result = {
+                                        data: eachData.val(),
+                                        whereHit: "content",
+                                        indexOf: content.search(searchFor)
+                                    };
+                                    resultOfSearch.push(result);
+
+                                } else {
+                                    console.log("not found");
+                                }
+                                if (typeof callback === 'function') {
+                                    callback(resultOfSearch);
+                                }
                             }
-                            if (typeof callback ==='function') {
-                                console.log(resultOfSearch);
-                                callback(resultOfSearch);
-                            }
+
                         })
                 })
             })
