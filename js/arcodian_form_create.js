d001cca94dc417628f6da2077d737bcc1f02d13b
Ghobe <ghobekang@gmail.com>
2018-01-21 12:17:20 +0900

    diary page init content loading bug fixed

--- js/arcodian_form_create.js
+++ js/arcodian_form_create.js
@@ -4,6 +4,9 @@
 var date = new Date();
 var year = date.getFullYear();
 var month = date.getMonth()+1;
+if (month < 10 ) {
+    month = "0" + month;
+}
 var counter= 0;
 
 functions.load("diary", year + "/" + month, function (data) {
